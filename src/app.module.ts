426d1f750dfd5ce8dc8a3247796098d8db84ee40
Chia1104 <yuyuchia7423@gmail.com>
2022-07-08 17:14:18 +0800

    :construction: Work in progress.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      buildSchemaOptions: { dateScalarMode: 'timestamp' },
     }),
     ProductsModule,
   ],
+  providers: [PrismaService],
 })
 export class AppModule {}
